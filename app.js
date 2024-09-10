aa8349bc9e8c82b141a3e2f020da1d1188c9c5d1
JeyVictor-Dev <jeyjeyvictor@gmail.com>
2024-09-09 23:23:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+    console.log(section);
+    
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value
+
+    //se o campoPesquisa for uma string sem nada
+    if (campoPesquisa.trim() === "") {
+        section.innerHTML = "<p>Nada foi encontrado</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let sinopse = "";
+    let tags = "";
+
+    //par cada dado dentro da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        sinopse = dado.sinopse.toLowerCase()
+        tags = dado.tags.toLowerCase()
+       //se titulo includes campuPesquisa
+       if (titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href=${dado.trailer}target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.sinopse}
+            </p>
+                <p class="descricao-meta">${dado.ano}
+                </p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+        </div>
+    `
+       }
+         //então, faça ....
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado. Você precisa digitar o nome de um filme do studio ghibli</p>"
+    }
+
+    section.innerHTML = resultados
+}
