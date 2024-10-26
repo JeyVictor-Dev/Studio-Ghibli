82841dd001ecb82cfbb52e831b9ab1b199e18f6d
JeyVictor-Dev <jeyjeyvictor@gamil.com>
2024-10-26 04:36:12 -0300

    css atualizado

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "A Viagem de Chihiro",
+        trailer: "https://youtu.be/ByXuk9QqQkk?si=jcbXYIDcHIgqSPw4",
+        sinopse: "A Viagem de Chihiro é uma obra-prima da animação japonesa, dirigida pelo renomado Hayao Miyazaki. A história acompanha Chihiro, uma menina que, durante uma mudança, se encontra em um mundo mágico e misterioso habitado por espíritos e criaturas fantásticas. Ao testemunhar a transformação de seus pais em porcos, a jovem embarca em uma jornada épica para salvá-los e encontrar o caminho de volta para casa.",
+        ano: 2001,
+        link: "https://www.adorocinema.com/filmes/filme-37485/",
+        tags: "chihiro chiriro porcos espiritos"
+    },
+    {
+        titulo: "Meu Amigo Totoro",
+        trailer: "https://youtu.be/92a7Hj0ijLs?si=o-bmSj3yPL0AGe_w",
+        sinopse: "Meu Amigo Totoro é uma animação japonesa que conta a história de duas irmãs que se mudam para o campo com o pai e lá encontram Totoro, uma criatura mágica e gentil que habita a floresta próxima. Uma jornada emocionante sobre a infância, a imaginação e a força da família.",
+        ano: 1995,
+        link: "https://www.adorocinema.com/filmes/filme-14790/",
+        tags:'totoro criatura magica floresta familia infancia'
+    },
+    {
+        titulo: "O Castelo Animado",
+        trailer: "https://youtu.be/iwROgK94zcM?si=pphUeasStkpfASfe",
+        sinopse: "O Castelo Animado é uma aventura mágica que acompanha Sophie, uma jovem que é transformada em uma velha por uma bruxa. Em busca de uma cura, ela se depara com um castelo ambulante e com o misterioso mago Howl.",
+        ano: 2004,
+        link: "https://www.adorocinema.com/filmes/filme-50409/",
+        tags:"velha bruxa howl castelo sophie"
+    }
+];
